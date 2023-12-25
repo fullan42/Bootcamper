0ce5482e1c26dc48f36f201ff96a631efabb3070
fullan42 <hasan_-1@outlook.com>
2023-12-25 19:50:44 +0300

    sd

---
+++ src/main/java/com/example/bootcamper/BootCamperApplication.java
@@ -0,0 +1,13 @@
+package com.example.bootcamper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootCamperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootCamperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bootcamper/BootCamperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bootcamper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootCamperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
