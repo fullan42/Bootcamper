b1254dfb0d346afeda9a749fab18e8b9c5d285e6
fullan42 <hasan_-1@outlook.com>
2024-01-08 18:59:13 +0300

    ilk

--- src/main/java/BootCamp/BootCamp.java
+++ src/main/java/BootCamp/BootCamp.java
@@ -1,10 +1,19 @@
 package BootCamp;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @Entity
 @AllArgsConstructor
+@NoArgsConstructor
 public class BootCamp {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
 }
